03188c29976268451bb5a9c21e26d1244dbeb230
rajan-personal <39906424+rajan-personal@users.noreply.github.com>
2024-08-27 00:02:33 +0530

    grid

---
+++ kit/src/lib/components/element.svelte
@@ -0,0 +1,3 @@
+<div class="p-5 border border-spacing-10 border-b-gray-950">
+    <slot/>
+</div>
---
+++ kit/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Element from "$lib/components/element.svelte";
+</script>
+
+
+<div class="h-screen w-full flex overflow-x-auto">
+    {#each Array(20).fill(1) as i} 
+    <div class="h-full min-w-[300px] flex-shrink-0 flex flex-col overflow-y-auto border-8">
+        {#each Array(100).fill(1) as i} 
+        <Element>
+            hello
+        </Element>
+        {/each}
+    </div>
+    {/each}
+</div>
