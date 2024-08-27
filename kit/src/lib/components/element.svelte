9b7f7249c077d50f15eee2e150b243b77379d983
rajan-personal <39906424+rajan-personal@users.noreply.github.com>
2024-08-27 11:02:46 +0530

    sticky scroll

--- kit/src/lib/components/element.svelte
+++ kit/src/lib/components/element.svelte
@@ -1,3 +1,19 @@
-<div class="p-5 border border-spacing-10 border-b-gray-950">
+<!-- <script>
+    export let selected = true;
+</script>
+
+<div class="p-5 border border-spacing-10 border-b-black" class:bg-light-blue-200={selected}>
+    <slot/>
+</div> -->
+
+<script>
+    export let selected = false;
+</script>
+
+<div 
+    class="p-5 border border-spacing-10 border-b-black" 
+    class:bg-blue-100={selected}
+    class:bg-opacity-70={selected}
+>
     <slot/>
 </div>
--- kit/src/routes/+page.svelte
+++ kit/src/routes/+page.svelte
@@ -1,16 +1,15 @@
-<script>
+<script lang="ts">
     import Element from "$lib/components/element.svelte";
 </script>
 
-
-<div class="h-screen w-full flex overflow-x-auto">
-    {#each Array(20).fill(1) as i} 
-    <div class="h-full min-w-[300px] flex-shrink-0 flex flex-col overflow-y-auto border-8">
-        {#each Array(100).fill(1) as i} 
-        <Element>
-            hello
-        </Element>
+<div class="h-screen w-full flex overflow-x-auto snap-x snap-mandatory">
+    {#each Array(20).fill(1) as _}
+        <div class="h-full min-w-[300px] flex-shrink-0 flex flex-col overflow-y-auto border-8 snap-start">
+        {#each Array(100).fill(1) as _, rowIndex}
+            <Element selected={false}>
+            Row {rowIndex + 1}
+            </Element>
         {/each}
-    </div>
+        </div>
     {/each}
 </div>
