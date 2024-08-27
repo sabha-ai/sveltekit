785624952192ac4a958cf75480a63cb8e1773f7a
rajan-personal <39906424+rajan-personal@users.noreply.github.com>
2024-08-27 11:30:16 +0530

    row scroll

--- kit/src/lib/components/element.svelte
+++ kit/src/lib/components/element.svelte
@@ -1,17 +1,9 @@
-<!-- <script>
-    export let selected = true;
-</script>
-
-<div class="p-5 border border-spacing-10 border-b-black" class:bg-light-blue-200={selected}>
-    <slot/>
-</div> -->
-
 <script>
     export let selected = false;
 </script>
 
 <div 
-    class="p-5 border border-spacing-10 border-b-black" 
+    class="p-5 border border-spacing-10 border-b-black snap-start" 
     class:bg-blue-100={selected}
     class:bg-opacity-70={selected}
 >
--- kit/src/routes/+page.svelte
+++ kit/src/routes/+page.svelte
@@ -4,10 +4,10 @@
 
 <div class="h-screen w-full flex overflow-x-auto snap-x snap-mandatory">
     {#each Array(20).fill(1) as _}
-        <div class="h-full min-w-[300px] flex-shrink-0 flex flex-col overflow-y-auto border-8 snap-start">
+        <div class="h-full min-w-[300px] flex-shrink-0 flex flex-col overflow-y-auto border-8 snap-start snap-y snap-mandatory">
         {#each Array(100).fill(1) as _, rowIndex}
             <Element selected={false}>
-            Row {rowIndex + 1}
+                Row {rowIndex + 1}
             </Element>
         {/each}
         </div>
